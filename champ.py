2ef5c530857b83b71427570660a2b07c0aa1110b
AndrewKalil <1541@holbertonschool.com>
2020-06-17 20:17:25 -0500

    done with elemental abilities

--- champ.py
+++ champ.py
@@ -2,15 +2,25 @@
 """"""
 from base import Base
 import json
+import random
 
 class Champ(Base):
+    T_dmg_done = 0
+    T_dmg_taken = 0
+    T_dmg = 0
+    CRIT = 0
+    reset = 0
+    energy = 200
+    energy_reset = energy
+
     def __init__(
-        self, name, race, gender, id=None, weapon="", armor="", champ_class="",
+        self, name, race, gender, element=None, id=None, weapon="", armor="", champ_class="",
         health=0, attack=0, defence= 0, magic=0, speed=0, dmg_reduction=0):
         self.champ_class = champ_class
         self.name = name
         self.race = race
         self.gender = gender
+        self.element = element
         self.health = health
         self.attack = attack
         self.defence = defence
@@ -19,6 +29,13 @@ class Champ(Base):
         self.armor = armor
         self.speed = speed
         self.dmg_reduction = dmg_reduction
+        self.T_dmg_done = Champ.T_dmg_done
+        self.T_dmg_taken = Champ.T_dmg_taken
+        self.T_dmg = Champ.T_dmg
+        self.CRIT = Champ.CRIT
+        self.reset = health
+        self.energy = Champ.energy
+        self.energy_reset = Champ.energy_reset
         super().__init__(id)
 
     @property
@@ -36,9 +53,12 @@ class Champ(Base):
 
     @race.setter
     def race(self, value):
-        self.validator('race', int(value))
-        race_list = ["Human", "Elf", "Dwarf", "Hobbit", "Orc"]
-        self.__race = str(race_list[int(value) - 1])
+        if type(value) is int:
+            self.validator('race', int(value))
+            race_list = ["Human", "Elf", "Dwarf", "Hobbit", "Orc"]
+            self.__race = str(race_list[int(value) - 1])
+        else:
+            self.__race = value
 
     @property
     def gender(self):
@@ -46,9 +66,25 @@ class Champ(Base):
 
     @gender.setter
     def gender(self, value):
-        self.validator('gender', int(value))
-        gender_list = ["Male", "Female", "Other"]
-        self.__gender = str(gender_list[int(value) - 1])
+        if type(value) is int:
+            self.validator('gender', int(value))
+            gender_list = ["Male", "Female", "Other"]
+            self.__gender = str(gender_list[int(value) - 1])
+        else:
+            self.__gender = value
+
+    @property
+    def element(self):
+        return self.__element
+
+    @element.setter
+    def element(self, value):
+        if type(value) is int:
+            self.validator('element', int(value))
+            element_list = ['Solar', 'Arc', 'Void']
+            self.__element = str(element_list[int(value) - 1])
+        else:
+            self.__element = value
 
     @property
     def speed(self):
@@ -56,14 +92,7 @@ class Champ(Base):
 
     @speed.setter
     def speed(self, value):
-        if self.armor == "heavy":
-            self.__speed = value + 3
-        elif self.armor == "medium":
-            self.__speed = value + 7
-        elif self.armor == "light":
-            self.__speed = value + 15
-        else:
-            self.__speed = value
+        self.__speed = value
 
     @property
     def dmg_reduction(self):
@@ -71,14 +100,7 @@ class Champ(Base):
 
     @dmg_reduction.setter
     def dmg_reduction(self, value):
-        if self.armor == "heavy":
-            self.__dmg_reduction = value + 6
-        elif self.armor == "medium":
-            self.__dmg_reduction = value + 3
-        elif self.armor == "light":
-            self.__dmg_reduction = value + 2
-        else:
-            self.__dmg_reduction = value
+        self.__dmg_reduction = value
 
     def validator(self, name, value):
         if name is 'name' and len(value) > 10:
@@ -87,11 +109,21 @@ class Champ(Base):
             raise Exception("No race was chosen")
         if name is 'gender' and (1 > value > 3):
             raise Exception("No gender was chosen")
+        if name is 'element' and (1 > value > 3):
+            raise Exception("No element was chosen")
+
+    def validate_list(self, name, value):
+        if name is 'race' and value not in ["Human", "Elf", "Dwarf", "Hobbit", "Orc"]:
+            raise Exception("Not a valid race")
+        if name is 'gender' and value not in ["Male", "Female", "Other"]:
+            raise Exception("Not a valid gender")
+        if name is 'element' and value not in ['Solar', 'Arc', 'Void']:
+            raise Exception("Not a valid element")
 
     def __str__(self):
-        str = "Class: {}\nName: {}\nRace: {}\nGender: {}\nHealth: {}\nAttack: {}\nDefence: {}\nMagic: {}\nWeapon: {}\nArmor: {}\nSpeed: {}\nDamage Reduction: {}%\nLevel: {}\nEXP for Next Level: {}\nCurrent EXP: {}\nTotal EXP: {}\nStat Points: {}\nChampion id: {}\n"
+        str = "Class: {}\nName: {}\nRace: {}\nGender: {}\nElement: {}\nHealth: {}\nAttack: {}\nDefence: {}\nMagic: {}\nWeapon: {}\nArmor: {}\nSpeed: {}%\nDamage Reduction: {}%\nLevel: {}\nEXP for Next Level: {}\nCurrent EXP: {}\nTotal EXP: {}\nStat Points: {}\nChampion id: {}\n"
         return (str.format(
-            self.champ_class, self.name, self.race, self.gender, self.health, self.attack, self.defence, self.magic, self.weapon,
+            self.champ_class, self.name, self.race, self.gender, self.element, self.health, self.attack, self.defence, self.magic, self.weapon,
             self.armor, self.speed, self.dmg_reduction, self.Level, self.EXP_N_L, self.C_EXP, self.T_EXP, self.S_Points, self.id))
 
     def to_dictionary(self):
@@ -102,12 +134,18 @@ class Champ(Base):
         """
         dic = {}
         ls = [
-            'champ_class', 'name', 'race', 'gender', 'health', 'attack', 'defence', 'magic',
+            'champ_class', 'name', 'race', 'gender', "element", 'health', 'attack', 'defence', 'magic',
             'weapon', 'armor', 'speed', 'dmg_reduction', 'Level', 'EXP_N_L', 'C_EXP', 'T_EXP', 'S_Points', 'id'
         ]
         for i in ls:
             dic[i] = getattr(self, i)
         return dic
+
+    def update(self, **kwargs):
+        """udates an object
+        """
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def save_to_file(self):
         """saves a json string to a file
@@ -122,22 +160,103 @@ class Champ(Base):
         self.S_Points += 3
         self.Level += 1
         self.EXP_N_L =  self.EXP_N_L + (self.EXP_N_L * self.Level)
-        self.T_EXP += self.C_EXP
+        self.T_EXP += self.EXP_N_L
         self.C_EXP = 0
+        self.energy += 50
+        self.energy_reset = self.energy
 
-    def gain_xp(self, dmg=0):
-        self.C_EXP = (0.5 * dmg) + 10
+    def gain_xp(self):
+        self.C_EXP = (0.5 * self.T_dmg) + 10
+        if self.C_EXP >= self.EXP_N_L:
+            self.level_up()
 
     def death(self):
         self.C_EXP -= (0.5 * self.C_EXP)
+        if self.C_EXP <= 0:
+            self.C_EXP = 0
 
-    def update(self, **kwargs):
+    def stat_reset(self):
+        self.health = self.reset
+        self.T_dmg = 0
+        self.energy = self.energy_reset
+
+    def increase_stats(self, **kwargs):
         """udates an object
         """
-        for key, value in kwargs.items():
-            if key in ['health', 'attack', 'defence', 'magic']:
-                value = getattr(self, key) + 3
-            elif key in ['speed', 'dmg_reduction']:
-                value = getattr(self, key) + .25
-            setattr(self, key, value)
-        self.S_Points -= len(kwargs)
+        if len(kwargs) <= self.S_Points:
+            for key, value in kwargs.items():
+                if key in ['health', 'attack', 'defence', 'magic']:
+                    value = getattr(self, key) + 5
+                    setattr(self, key, value)
+                elif key in ['armor']:
+                    value_speed = getattr(self, "speed") + 0.5
+                    setattr(self, "speed", value_speed)
+                    value_dmg_reduction = getattr(self, "dmg_reduction") + 0.5
+                    setattr(self, "dmg_reduction", value_dmg_reduction)
+                else:
+                    print("You cannot access that attribute")
+            self.S_Points -= len(kwargs)
+        else:
+            print("You can only use the attribute points available")
+        self.reset = self.health
+
+    def attack_action(self, enemy=None):
+        value = random.randint(1, 100)
+        if 1 <= value <= self.speed:
+            self.CRIT = (self.speed / 100)
+        if self.champ_class in ["Fighter", "Rogue"]:
+            self.T_dmg_done = (.60*self.attack) + (self.CRIT * self.attack)
+        elif self.champ_class in ["Mage"]:
+            self.T_dmg_done = (.60*self.magic) + (self.CRIT * self.magic)
+        if self.element is "Solar" and enemy is "Arc":
+            value = self.T_dmg_done + (self.T_dmg_done * .25)
+        elif self.element is "Arc" and enemy is "Void":
+            value = self.T_dmg_done + (self.T_dmg_done * .25)
+        elif self.element is "Void" and enemy is "Solar":
+            value = self.T_dmg_done + (self.T_dmg_done * .25)
+        else:
+            value = self.T_dmg_done
+        self.T_dmg += value
+        self.energy -= 5 + (value * .20)
+        if self.energy <= 0:
+            self.energy = 0
+        return value
+
+    def defend_action(self, dmg):
+        value = ((self.defence/100)*dmg) + ((self.dmg_reduction/100)*dmg)
+        return value
+
+    def total_defence(self, dmg, enemy=None):
+        if self.element is "Solar" and enemy is "Void":
+            value = dmg + (dmg * .25)
+        elif self.element is "Arc" and enemy is "Solar":
+            value = dmg + (dmg * .25)
+        elif self.element is "Void" and enemy is "Arc":
+            value = dmg + (dmg * .25)
+        else:
+            value = dmg
+        rand = random.randint(1, 100)
+        if 1 <= rand <= self.speed:
+            self.T_dmg_taken = 0
+        else:
+            self.T_dmg_taken = value - self.defend_action(dmg)
+        self.health -= self.T_dmg_taken
+        if self.health <= 0:
+            self.health = 0
+
+
+    def display_battle_stats(self):
+        str = "Name: {}\nHealth: {:.2f}\nEnergy: {:.2f}"
+        if self.health <= 0:
+            self.health = 0
+        print(str.format(self.name, float(self.health), float(self.energy)))
+
+    def win(self):
+        self.gain_xp()
+        self.stat_reset()
+        self.save_to_file()
+
+    def lose(self):
+        self.death()
+        self.stat_reset()
+        self.save_to_file()
--- main.py
+++ main.py
@@ -5,9 +5,11 @@ from fighter import Fighter
 from mage import Mage
 from rogue import Rogue
 from champ import Champ
+from base import Base
+from time import sleep
 
 if __name__ == '__main__':
-  name = input("Enter the name of the Champion (Less that 10 characters): ")
+  """name = input("Enter the name of the Champion (Less that 10 characters): ")
   print()
 
   race = input("Choose the race of your champion:\n1) Human\n2) Elf\n3) Dwarf\n4) Hobbit\n5) Orc\nChoose number: ")
@@ -25,15 +27,61 @@ if __name__ == '__main__':
   elif champ_class is 2:
     func = Mage
   elif champ_class is 3:
-    func = Rogue
+    func = Rogue"""
 
-  h1 = func(name, race, gender)
-  print(h1, end="\n")
-  h1.level_up()
-  print(h1, end="\n")
-  h1.update(health=1, attack=1, defence=1)
-  print(h1)
-  h1.level_up()
-  print(h1, end="\n")
-  h1.update(speed=1, dmg_reduction=1, attack=1)
-  print(h1, end="\n")
+  """dic = Fighter.load_from_file("Andrew")
+  p1 = Fighter("Dummy", 1, 1)
+  p1.update(**dic)
+  p1.level_up()
+  p1.increase_stats(health=1, defence=1, armor=1)"""
+  p1 = Mage("Andrew", "Human", "Male", "Solar")
+  p1.update(Level=10)
+  print(p1, end="\n")
+  print(p1.attack_action())
+  print(p1.soul_burn("Arc"))
+  print(p1.empowered_staff("Arc"))
+  print(p1.legion_attack("Arc"))
+  print(p1.ultimate("Arc"))
+  print()
+  print(p1.attack_action())
+  print(p1.soul_burn())
+  print(p1.empowered_staff())
+  print(p1.legion_attack())
+  print(p1.ultimate())
+  """p1.save_to_file()"""
+  """p1 = Fighter("Andrew", 1, 1)
+  p2 = Rogue("Sasuke", 1, 1)
+  print(p1, end="\n")
+  print(p2, end="\n")"""
+
+  """while True:
+    if p1.health <= 0 or p2.health <= 0:
+      if p1.health > 0:
+        print("PLAYER 1 WINS")
+        p1.win()
+        p2.lose()
+      elif p2.health > 0:
+        print("PLAYER 2 WINS")
+        p2.win()
+        p1.lose()
+      break
+
+    if p1.health > 0:
+      print("Player 1 Attacking...")
+      sleep(3)
+      dmg1 = p1.attack_action()
+      p2.defend_action(dmg1)
+      print("Player 1 stats after attack:")
+      p1.display_battle_stats()
+      print("Player 2 stats after attack")
+      p2.display_battle_stats()
+
+    if p2.health > 0:
+      print("Player 2 Attacking...")
+      sleep(3)
+      dmg2 = p2.attack_action()
+      p1.defend_action(dmg2)
+      print("Player 1 stats after attack:")
+      p1.display_battle_stats()
+      print("Player 2 stats after attack")
+      p2.display_battle_stats()"""
